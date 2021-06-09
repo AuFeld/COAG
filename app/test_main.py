058d7eaa636477aabab88451558a8fe51f6cc5fb
AuFeld <goldfeld.chase@gmail.com>
2021-06-09 11:06:28 -0400

    update

--- app/test_main.py
+++ app/test_main.py
@@ -6,6 +6,6 @@ client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get("/")
+    response = client.get("/", tags=["Root"])
     assert response.status_code == 200
     assert response.json() == {"message": "Wecome to COAG!"}
