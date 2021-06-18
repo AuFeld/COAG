e72c2d09d14893c6bdbf5347a532e025caca3ebc
AuFeld <goldfeld.chase@gmail.com>
2021-06-18 10:10:48 -0400

    created indeed data model

---
+++ app/models/indeed_model.py
@@ -0,0 +1,37 @@
+from datetime import date
+from typing import Optional
+from pydantic import BaseModel, Field
+from pydantic.types import date
+
+class Indeed(BaseModel): 
+    id: str = Field(...)
+    jobtitle: Optional[str] = Field(...)
+    company: Optional[str] = Field(...)
+    location: Optional[str] = Field(...)
+    summary: Optional[str] = Field(...)
+    requirements: Optional[str] = Field(...)
+    description: Optional[str] = Field(...)
+    state: Optional[str] = Field(...)
+    city: Optional[str] = Field(...)
+    dateposted: Optional[date] = Field(...)
+    schedule: Optional[str] = Field(...)
+    salary: Optional[str] = Field(...)
+    role: Optional[str] = Field(...)
+    focus: Optional[str] = Field(...)
+
+
+    '''
+    :orm_mode: 
+    tells pydantic to read the data even if it's not a dict, 
+    but an ORM model or any other arbitrary object w/ attributes
+    '''
+    class Config: 
+        orm_mode = False,
+        schema_extra = {
+            "example": {
+                "jobtitle": "Software Engineer",
+                "company": "Hooli",
+                "city": "Palo Alto",
+                "state": "CA",
+            }
+        }
